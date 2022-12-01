60b864ca083bc326837f6816d55d3dec5dd2a0c6
Kira Samsonova <e.p.samsonov@diacare-soft.ru>
2022-12-01 03:12:54 +0300

    kanban board initial commit

--- app/src/main.js
+++ app/src/main.js
@@ -11,10 +11,12 @@ import KanbanModule from './modules/KanbanModule'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/ru-RU'
+import vueKanban from 'vue-kanban'
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI, { locale })
-
+Vue.use(vueKanban)
 Vue.use(MainModule, { store, router })
 Vue.use(IssuesModule, { store, router })
 Vue.use(KanbanModule, { store, router })
--- app/src/modules/KanbanModule/pages/kanban/index.vue
+++ app/src/modules/KanbanModule/pages/kanban/index.vue
@@ -1,18 +1,30 @@
 <template>
   <main-layout>
-<in-development></in-development>
+    <kanban-board
+      :stages="stages"
+      :blocks="blocks"
+      @update-block="updateBlock"
+      >
+      </kanban-board>
   </main-layout>
 </template>
 
 <script>
 import MainLayout from '@/layouts/Main'
-import InDevelopment from '@/components/InDevelopment'
 import localization from '@/modules/MainModule/pages/main/localization.json'
 export default {
   name: 'KanbanPage',
-  components: { InDevelopment, MainLayout },
+  components: { MainLayout },
   data () {
     return {
+      stages: ['on-hold', 'in-progress', 'needs-review', 'approved'],
+      blocks: [
+        {
+          id: 1,
+          status: 'on-hold',
+          title: 'Test'
+        }
+      ]
     }
   },
   computed: {
@@ -21,7 +33,9 @@ export default {
     }
   },
   methods: {
-
+    updateBlock (id, status) {
+      this.blocks.find(b => b.id === Number(id)).status = status
+    }
   },
   mounted () {
   }
