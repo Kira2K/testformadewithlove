6dd0afd5000e8242ded443b34a40baf49001c083
Kira Samsonova <e.p.samsonov@diacare-soft.ru>
2022-12-02 08:46:25 +0300

    make kanban page work with store customize default kanban component

--- app/src/modules/KanbanModule/pages/kanban/index.vue
+++ app/src/modules/KanbanModule/pages/kanban/index.vue
@@ -10,47 +10,71 @@
       </el-button>
     <kanban-board
       :stages="stages"
-      :blocks="blocks"
+      :blocks="getIssues"
       @update-block="updateBlock"
       >
+      <div
+        v-for="block in getIssues"
+        :slot="block.id"
+        :key="block.id"
+        class="drag-item__inner">
+        <div class="drag-item__header">
+          <router-link
+            :to="'/issues/'+block.id"
+            class="drag-item__header"
+            >
+            <span>
+              {{l10n.task}} {{ block.id }}
+            </span>
+          </router-link>
+        </div>
+        <div class="drag-item__body">
+          <p class="drag-item__title">{{ block.title }}</p>
+        </div>
+      </div>
+    </kanban-board>
   </main-layout>
 </template>
 
-<script>
-import MainLayout from '@/layouts/Main'
+<script lang="ts">
+import MainLayout from '@/layouts/Main/index.vue'
 import localization from './localization.json'
-export default {
+import { mapGetters, mapActions } from 'vuex'
+import { ChangeIssueStatusByIdArg } from '@/types'
+import Vue from 'vue'
+export default Vue.extend({
   name: 'KanbanPage',
   components: { MainLayout },
   data () {
     return {
-      stages: ['on-hold', 'in-progress', 'needs-review', 'approved', 'production'],
-      blocks: [
-        {
-          id: 1,
-          status: 'on-hold',
-          title: 'Test'
-        }
-      ]
+      stages: ['on-hold', 'in-progress', 'needs-review', 'approved', 'production']
     }
   },
+
   computed: {
-    l10n () {
+    ...mapGetters('issues', [
+      'getIssues'
+    ]),
+    l10n ():Object {
       return localization[this.$lang]
     }
   },
   methods: {
-    updateBlock (id, status) {
-      this.blocks.find(b => b.id === Number(id)).status = status
+    ...mapActions('issues', [
+      'changeIssueStatusById'
+    ]),
+    updateBlock (id, status):void {
+      const args:ChangeIssueStatusByIdArg = { id: Number(id), status }
+      this.changeIssueStatusById(args)
     },
-    createNewIssue () {
-      const text = this.l10n.availableNext
-      this.$setTextShowServerError(text)
+    createNewIssue ():void {
+      // const text = this.l10n.availableNext
+      this.$setTextShowServerError('text')
     }
   },
   mounted () {
   }
-}
+})
 </script>
 
 <style scoped lang="scss" src="./style.scss" />
